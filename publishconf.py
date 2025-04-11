ce8ba3404a973acb65e688061100dbe0bc702fe6
rvibek <rvibek@gmail.com>
2025-04-11 10:37:45 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
+SITEURL = "https://glatis-dk.github.io/koen-p--den-glatte-is/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
